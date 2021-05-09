ea8c4a042b11719443789d163f6237ff2d582c40
Konstantin Knyazev <29006524+sckv@users.noreply.github.com>
2021-05-09 16:57:56 +0200

    feat(sever): request flow class, middlewares apply event stream

--- src/request.ts
+++ src/request.ts
@@ -1,9 +1,13 @@
 import type {
   IncomingHttpHeaders,
   IncomingMessage,
+  OutgoingHttpHeader,
+  OutgoingHttpHeaders,
   ServerResponse,
 } from 'http';
 import type { Socket } from 'net';
+import { Writable } from 'stream';
+import { uuidv4, StatusCodes, StatusPhrases } from './utils/';
 
 type WebRequest = {
   headers: IncomingHttpHeaders;
@@ -12,7 +16,7 @@ type WebRequest = {
   complete: boolean;
   destroyed: boolean;
   destroy(error?: Error | undefined): void;
-  pipe<T>(
+  pipe<T extends NodeJS.WritableStream>(
     destination: T,
     options?:
       | {
@@ -24,12 +28,12 @@ type WebRequest = {
 };
 
 type WebResponse = {
-  socket: Socket;
+  socket: Socket | null;
   statusCode: number;
   headersSent: boolean;
   chunkedEncoding: boolean;
   destroyed: boolean;
-  pipe<T>(
+  pipe<T extends NodeJS.WritableStream>(
     destination: T,
     options?:
       | {
@@ -39,11 +43,82 @@ type WebResponse = {
   ): T;
   shouldKeepAlive: boolean;
   on: ServerResponse['on'];
+  end: Writable['end'];
+  writeHead(
+    statusCode: number,
+    reasonPhrase?: string | undefined,
+    headers?: OutgoingHttpHeaders | OutgoingHttpHeader[] | undefined,
+  ): void;
+  addTrailers(headers: OutgoingHttpHeaders | readonly [string, string][]): void;
 };
 
+const requestKeys = [
+  'headers',
+  'rawHeaders',
+  'aborted',
+  'complete',
+  'destroyed',
+  'destroy',
+  'pipe',
+  'on',
+] as const;
+const responseKeys = [
+  'socket',
+  'statusCode',
+  'headersSent',
+  'chunkedEncoding',
+  'destroyed',
+  'pipe',
+  'shouldKeepAlive',
+  'on',
+  'end',
+  'writeHead',
+  'addTrailers',
+] as const;
+
+const pickFrom = <O, T extends Array<keyof O>>(obj: O, ...args: T) => {
+  return args.reduce((acc, curr) => {
+    acc[curr] = obj[curr];
+    return acc;
+  }, {} as { [K in keyof O]: O[K] });
+};
 export class RequestFlow {
-  constructor(
-    private request: IncomingMessage,
-    private response: ServerResponse,
-  ) {}
+  request: WebRequest;
+  response: WebResponse;
+  uuid: string;
+  private status: number | undefined;
+
+  constructor(public originalRequest: IncomingMessage, public originalResponse: ServerResponse) {
+    this.request = pickFrom(originalRequest, ...requestKeys);
+    this.response = pickFrom(originalResponse, ...responseKeys);
+    this.uuid = (originalRequest.headers['x-request-id'] as string) || uuidv4();
+  }
+
+  setHeader(header: string, value: string) {
+    this.originalResponse.setHeader(header, value);
+    return this;
+  }
+
+  setHeaders(headers: { [header: string]: string }) {
+    Object.entries(headers).forEach(([header, value]) => {
+      this.originalResponse.setHeader(header, value);
+    });
+    return this;
+  }
+
+  setStatus(status: typeof StatusCodes[keyof typeof StatusCodes]) {
+    this.status = status;
+  }
+
+  sendStatus(status: typeof StatusCodes[keyof typeof StatusCodes]) {
+    this.setStatus(status);
+    this.send(StatusPhrases);
+  }
+
+  send(chunk: any) {
+    this.originalResponse.write(chunk, 'utf-8', (e) => {
+      if (e) throw e;
+    });
+    this.originalResponse.end();
+  }
 }
