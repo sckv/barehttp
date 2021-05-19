a676c7541119ad6f8966f7fd963f4185cd082791
Konstantin Knyazev <29006524+sckv@users.noreply.github.com>
2021-05-19 23:27:08 +0200

    feat(server): receive and decode basic body

--- src/utils/safe-json.ts
+++ src/utils/safe-json.ts
@@ -2,7 +2,16 @@ export const JSONStringify = (data: any) => {
   try {
     return JSON.stringify(data);
   } catch (e) {
-    console.log('Error stringifying', e);
-    return 'Not serializable';
+    console.log('Error stringifying, data not serializable', e);
+    return null;
+  }
+};
+
+export const JSONParse = (data: any) => {
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    console.log('Error parsing, data not deserializable', e);
+    return null;
   }
 };
