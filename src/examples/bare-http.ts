4a6da78d28a020f20d486419d4c7e8a64b1b5323
Konstantin Knyazev <29006524+sckv@users.noreply.github.com>
2021-05-29 01:52:31 +0200

    feat(fast-json): wip type walking

--- src/examples/bare-http.ts
+++ src/examples/bare-http.ts
@@ -2,15 +2,30 @@ import { BareHttp, logMe, context } from '../index';
 
 const app = new BareHttp({ cookies: true });
 
+interface Ok {
+  haha: number;
+}
+type SomeReturning = {
+  fine: string;
+  coolData: number;
+  kek?: Ok;
+};
+
+const returning: SomeReturning = {
+  fine: 'fine',
+  coolData: 1123,
+  kek: { haha: 123123 },
+};
+
 const wait = () => new Promise((resolve) => setTimeout(resolve, 5000));
 app.route.get({
   route: '/route',
   options: { timeout: 2000 },
-  handler: async (flow) => {
+  handler: async function (flow) {
     flow.cm?.setCookie('MY KOOKIE', 'value', { domain: 'localhost' });
     await wait();
     // throw new Error('just fail');
-    return 'JUST MESSAGE 2';
+    return returning;
   },
 });
 
