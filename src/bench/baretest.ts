a7b5f66b29658a51b971bee8eead9ff36386170e
Konstantin Knyazev <konstantinknyazev@outlook.com>
2021-08-01 15:51:17 +0200

    feat(ws): add websocket handy methods exposure for easy interaction

--- src/bench/baretest.ts
+++ src/bench/baretest.ts
@@ -1,12 +1,23 @@
 import { BareHttp } from '../index';
 
-const brt = new BareHttp({ statisticsReport: true });
+const brt = new BareHttp({ statisticsReport: true, ws: true });
 
 brt.get({
   route: '/myroute',
   handler: (flow) => {
     flow._send('just response');
   },
+});
+
+let clt = 0;
+
+brt.ws?.declareReceiver<{ ok: string }, number>('BASE.TYPE', async (data, client) => {
+  console.log({ data });
+  return { cool: 'some answer', client };
+});
+
+brt.ws?.defineUpgrade(async (req) => {
+  return { access: true, client: ++clt };
 });
 
 brt.start(() => console.log('server started'));
